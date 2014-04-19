5fe352dd075fb2329986c995c8bc3ca13738750f
Cary Lee <carylee@gmail.com>
2014-04-19 13:23:01 -0700

    Got to-short-array working

--- src/barktracker_client/core.clj
+++ src/barktracker_client/core.clj
@@ -1,6 +1,8 @@
 (ns barktracker-client.core
   [:require [clj-http.client :as client]]
-  [:import (javax.sound.sampled AudioFormat AudioSystem DataLine DataLine$Info LineUnavailableException TargetDataLine)])
+  [:import [javax.sound.sampled AudioFormat AudioSystem DataLine DataLine$Info LineUnavailableException TargetDataLine]
+           [java.nio Buffer ByteBuffer ByteOrder]
+           [java.io File]])
 
 (defn rms [xs]
   (Math/sqrt (/ (reduce + (map #(* % %) xs))
@@ -15,6 +17,9 @@
       (.start))
     line))
 
+(defn file-line [file]
+  (AudioSystem/getAudioInputStream (File. file)))
+
 (defn log-bark [start stop]
   (do
     (println (str "Start: " start ", stop: " stop))
@@ -22,7 +27,34 @@
       (client/post "http://localhost:3000/barks" {:form-params {:start start :stop stop} :throw-exceptions false})
       (catch Exception ex))))
 
-(defn listen-for-barks [threshold]
+(defn listen-for-barks [threshold line buffer]
+  ;(let [line (open-line audio-format)
+        ;buffer (byte-array (/ (.getBufferSize line) 5))]
+    (loop [bark-start-time 0]
+      (.read line buffer 0 (count buffer))
+      (let [is-barking (> (rms buffer) threshold)
+            was-barking (< 0 bark-start-time)]
+        (cond
+          (= was-barking is-barking) (recur bark-start-time)
+          is-barking (recur (System/currentTimeMillis))
+          :else (do (log-bark bark-start-time (System/currentTimeMillis)) (recur 0))))))
+
+(defn listen-for-barks-mic [threshold]
+  (let [line (open-line audio-format)
+        buffer (byte-array (/ (.getBufferSize line) 5))]
+    (listen-for-barks threshold line buffer)))
+
+(defn listen-for-barks-file [threshold file]
+  (let [line (file-line file)
+        buffer (byte-array 1000)]
+    (listen-for-barks threshold line buffer)))
+
+(defn to-short-array [in-buffer]
+  (let [out-buffer (short-array (/ (count in-buffer) 2))]
+    (-> in-buffer ByteBuffer/wrap (.order ByteOrder/LITTLE_ENDIAN) .asShortBuffer (.get out-buffer))
+    out-buffer))
+
+(defn listen-for-barks-recording [threshold]
   (let [line (open-line audio-format)
         buffer (byte-array (/ (.getBufferSize line) 5))]
     (loop [bark-start-time 0]
@@ -35,4 +67,4 @@
           :else (do (log-bark bark-start-time (System/currentTimeMillis)) (recur 0)))))))
 
 (defn -main []
-  (listen-for-barks 4))
+  (listen-for-barks-mic 4))
