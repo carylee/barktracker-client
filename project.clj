faea73607221868bd10f2e9463f7a0b791a56a37
Cary Lee <carylee@gmail.com>
2014-04-20 23:42:39 -0700

    Got audio input working with 16 bit depth

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [incanter/incanter-core "1.5.4"]
+                 [incanter/incanter-charts "1.5.4"]
                  [clj-http "0.9.1"]]
   :main barktracker-client.core)
--- src/barktracker_client/core.clj
+++ src/barktracker_client/core.clj
@@ -4,11 +4,16 @@
            [java.nio Buffer ByteBuffer ByteOrder]
            [java.io File]])
 
+(use '(incanter core charts))
+(def threshold 2000)
+(def buffer-size 8820)
+;(def buffer (short-array buffer-size))
+
 (defn rms [xs]
   (Math/sqrt (/ (reduce + (map #(* % %) xs))
            (count xs))))
 
-(def audio-format (AudioFormat. 44100 8 1 true true))
+(def audio-format (AudioFormat. 44100 8 1 true false))
 
 (defn open-line [audio-format]
   (let [^TargetDataLine line (AudioSystem/getLine (DataLine$Info. TargetDataLine audio-format))]
@@ -19,6 +24,45 @@
 
 (defn file-line [file]
   (AudioSystem/getAudioInputStream (File. file)))
+
+(defn to-short-array
+  [in-buffer out-buffer]
+    (-> in-buffer ByteBuffer/wrap (.order ByteOrder/LITTLE_ENDIAN) .asShortBuffer (.get out-buffer))
+    out-buffer)
+
+(defn read-into-buffer
+  [line in-buffer out-buffer]
+   (.read line in-buffer 0 buffer-size)
+   (to-short-array in-buffer out-buffer)
+   out-buffer)
+
+(defn get-audio
+  ([seconds]
+   (let [line (open-line audio-format)]
+     (get-audio line seconds)))
+  ([line seconds]
+   (let [in-buffer (byte-array buffer-size)
+         out-buffer (short-array (/ buffer-size 2))]
+     (flatten (repeatedly (* seconds 10) #(vec (read-into-buffer line in-buffer out-buffer)))))))
+
+(defn chart-audio [seconds]
+  (let [audio (get-audio seconds)]
+    (view (xy-plot (range (count audio)) audio))))
+
+(defn print-when-loud [audio]
+  (if (> (rms audio) threshold) (println "Loud!")))
+
+(defn microphone-stream [action]
+  (let [in-buffer (byte-array buffer-size)
+        out-buffer (short-array (/ buffer-size 2))
+        line (open-line audio-format)]
+    (doseq [audio-window (repeatedly #(read-into-buffer line in-buffer out-buffer))] (action audio-window))))
+
+(defn read-entire-file [filename]
+  (let [ais (file-line filename)
+        byte-buffer (byte-array (.available ais))]
+    (.read ais byte-buffer)
+    (to-short-array byte-buffer)))
 
 (defn log-bark [start stop]
   (do
@@ -49,10 +93,21 @@
         buffer (byte-array 1000)]
     (listen-for-barks threshold line buffer)))
 
-(defn to-short-array [in-buffer]
-  (let [out-buffer (short-array (/ (count in-buffer) 2))]
-    (-> in-buffer ByteBuffer/wrap (.order ByteOrder/LITTLE_ENDIAN) .asShortBuffer (.get out-buffer))
-    out-buffer))
+(defn read-from-line [line]
+  (let [byte-buffer (byte-array buffer-size)]
+    (.read line byte-buffer)
+    (to-short-array byte-buffer)))
+
+;(defn read-entire-line [line]
+  ;(loop [data []]
+    ;(cond
+    ;(recur (conj data (read-from-line line)))))
+
+;(defn listen-for-barks [line]
+  ;(loop [bark-start-time 0]
+    ;(let [buffer (read-from-line buffer)
+          ;is-barking (> (rms buffer) threshold)]
+         ;buffer (read-from-line line)]
 
 (defn listen-for-barks-recording [threshold]
   (let [line (open-line audio-format)
